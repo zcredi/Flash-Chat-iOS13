8044c6646a9c65d78d9a216f58cf670dd28df3fd
zcredi <zcredi@gmail.com>
2022-12-21 18:32:32 +0300

    add tableView and DataSorce

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Владислав on 21.12.2022.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
